c80601f9a6ae25055db466e1093f2f2646aec3f3
marginalfriend <abdullahpsg@gmail.com>
2024-06-24 19:56:41 +0700

    [NEW] delete request handler

--- src/main.rs
+++ src/main.rs
@@ -170,3 +170,20 @@ fn handle_put_request(request: &str) -> (String, String) {
         _ => (INTERNAL_ERROR.to_string(), "Internal error".to_string()),
     }
 }
+
+//handle delete request
+fn handle_delete_request(request: &str) -> (String, String) {
+    match (get_id(&request).parse::<i32>(), Client::connect(DB_URL, NoTls)) {
+        (Ok(id), Ok(mut client)) => {
+            let rows_affected = client.execute("DELETE FROM users WHERE id = $1", &[&id]).unwrap();
+
+            //if rows affected is 0, user not found
+            if rows_affected == 0 {
+                return (NOT_FOUND.to_string(), "User not found".to_string());
+            }
+
+            (OK_RESPONSE.to_string(), "User deleted".to_string())
+        }
+        _ => (INTERNAL_ERROR.to_string(), "Internal error".to_string()),
+    }
+}
