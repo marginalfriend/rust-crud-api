e38a3a10f96ca87459b9250d38c653647ff27d14
marginalfriend <abdullahpsg@gmail.com>
2024-06-24 16:59:37 +0700

    [NEW] user struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use postgres::{ Client, NoTls };
+use postgres::Error as PostgresError;
+use std::net::{ TcpListener, TcpStream };
+use std::io::{ Read, Write };
+use std::env;
+
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: Option<i32>
+    name: String,
+    email: String,
+}
+
 fn main() {
-    println!("Hello, world!");
 }
